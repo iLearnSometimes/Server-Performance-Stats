ed383c0c930dff216bdf627f70f5a0a196c6357a
Marquis <wooten2489@gmail.com>
2025-07-14 19:33:49 -0400

    start of script

--- server-stats.sh
+++ server-stats.sh
@@ -3,3 +3,13 @@
 # Hello everyone, this is a practice script to analyse the performance of the server when it's called.
 #Testing get push & pull
 #last check
+
+main() {
+    get_top_cpu_processes
+}
+#Function to get top CPU Usage
+get_top_cpu_processes() {
+    echo "Top 5 Processes by CPU Usage:"
+    
+
+}
